fb7df332abeacebee16e725609fb8744c4a2d493
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 13:31:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-res.send(reateTemplate(articleone));
+res.send(createTemplate(articleone));
 });
 app.get('/article-two', function (req, res)
 {
