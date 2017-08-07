1a01fac529f7cf9b2c3052a23684726d9bdb5079
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 22:39:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //to move the object
-var img = document.getElementById('madi');
+var img = document.getElementById("madi");
 var marginLeft = '0';
 
 img.onClick = function(){
