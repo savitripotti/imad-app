401b3fbc6a56bc35d2d6cc936cb51028aa985298
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 14:02:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res){
     //articleName=article-one
     //articles[articleNmae]={} content for article-one
-    var articleName = req.params.artileName;
+    var articleName = req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
 
