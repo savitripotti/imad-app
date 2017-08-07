9b1453acbeba0bd7e06a3e0948bb04c24d654ea8
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 10:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one', function (req, res){
+app.get('/article-one', function (req, res){
     res.send('Article one request and will be served here');
 });
-app.get('/article two', function (req, res)
+app.get('/article-two', function (req, res)
 {
     res.send('Article two request and will be served here');
 });
-app.get('/article three', function (req, res)
+app.get('/article-three', function (req, res)
 {
     res.send('Article three request and will be served here');
 });
