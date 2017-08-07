0edea179752d0a70eebeae90d411db1e4115629c
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 20:30:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,15 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML="new value";
 //to move the object
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
-var img = document.getElementById('madi');
+
 img.onClick = function(){
     var interval = setInterval(moveRight, 100);
+
 
 };
