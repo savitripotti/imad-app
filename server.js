da2492244bb562c9171899396ef229c7daa0604a
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-07 13:30:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,51 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone= 
+{
+    title: 'articleone',
+    heading: 'articleone',
+    date: '7-8-2016',
+    content: '<p>this is my app</p>'
+    
+};
+function createTemplate (data){
+     var title= data.title;
+     var heading= data.heading;
+     var date= data.date;
+     var content= data.content;
+
+var htmlTemplate= `
+            <html>
+             <head>
+       <title>${tilte}</title>
+        <meta name="viexport" content="width-device-width, initial-scale=1">
+         <link href="/ui/style.css" rel="stylesheet" />
+
+            </head>
+    <body>
+        <div class="container">
+                <div>
+                    <a href='/'>home</a>
+                </div>
+                <h3>${heading}</h3>
+                <div>
+                ${date}
+                </div>
+                <div>
+                    ${content}
+            </div>
+    </div>
+</body>
+</html>`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(reateTemplate(articleone));
 });
 app.get('/article-two', function (req, res)
 {
