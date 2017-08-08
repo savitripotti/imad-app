969aeb215e1ab6baf39e78903bcc650dfbca5633
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-08 20:40:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
-console.log('Loaded!');
+  var button = document.getElementById('counter');
 
-       
+    var counter = 0;   
+      button.onclick = function(){
+          counter = counter + 1;
+           var span = document.getElemntById('count');
+             span.innerHTML = counter.toString();
+      };
