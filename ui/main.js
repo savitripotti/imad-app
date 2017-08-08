a3ca53ce22d17d51439aef7a93b7a637b7b4f6b5
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-08 20:43:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,17 @@
-  var button = document.getElementById('counter');
-
-    var counter = 0;   
-      button.onclick = function(){
+     var button = document.getElementById('counter');
+        button.onclick = function(){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(request.readystate === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200){
+                    var counter = request.responseText;
+                    var span = document.getElemntById('count');
+                    span.innerHTML = counter.toString();
+                }
+            }
+        };
+        request.open('GET','http://venisavitri.imad.hasura-app.io/counter',true);
+        request.send(null);
         
-             counter = counter + 1;
-           var span = document.getElemntById('count');
-             span.innerHTML = counter.toString();
-         }
-      };
+        };
