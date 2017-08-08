f9c008f830eb094528f4678103f640661525ab0f
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-08 10:57:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,12 @@ console.log('Loaded!');
 
 
     var img = document.getElementById("madi");
-    var marginLeft = 0;
+    
     img.onclick = function() {
-        setInterval( function() {
-            marginLeft += 5;
-            img.style.marginLeft = marginLeft.toString() + "px";
-        } , "50")};
+     function() {
+        
+            img.style.marginLeft = "100px";
+}
+};
     
 
