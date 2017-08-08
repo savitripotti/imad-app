e26017b57b2b32c5326d333fe9acb8b1e8dd8060
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-08 21:02:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,16 @@
      console.log('successful');
-     var button = document.getElementById('counter');
-        button.onclick = function(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-            if(request.readystate === XMLHttpRequest.DONE)
-            {
-                if(request.status === 200){
-                    var counter = request.responseText;
-                    var span = document.getElemntById('count');
-                    span.innerHTML = counter.toString();
-                }
-            }
-        };
-        request.open('GET','http://venisavitri.imad.hasura-app.io/counter',true);
-        request.send(null);
-        
-        };
+    console.log("JS Loaded");
+            var button = document.getElementById('counter');
+            var counter = 0;
+            
+             button.onclick = function() {
+              //make a request to counter endpoint
+               //capture the response and store it in a variable
+               //render the variable in correct span
+
+              counter = counter + 1;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+              };
+        console.log('ok');
+    
