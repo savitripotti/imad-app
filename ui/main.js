adf54cef256b6b41cc13e5a4a5db04d1fd56ce1a
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-09 19:17:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@
         submit.onclick=function(){
              var request = new XMLHttpRequest();
         request.onreadystatechange = function(){
-            if(request.readystate === XMLHttpRequest.DONE)
+            if(request.readyState === XMLHttpRequest.DONE)
             {
                 if(request.status === 200){
                   var names = request.responseText;
