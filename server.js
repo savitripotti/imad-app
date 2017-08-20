1f614ae4da393c2a41324868b2f60d1905ec0d8a
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-20 10:10:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/articles/:articleName', function (req, res){
            }
            else{
                var articleData = result.rows[0];
-               res.send(createTempate(articleData));
+               res.send(createTemplate(articleData));
            }
        }
    });
