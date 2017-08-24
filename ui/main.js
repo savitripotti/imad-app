eebae0ce4c8940f1a9695b3f02763ab2adb2c674
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-24 17:25:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,6 @@
                }     
                 }
         };
-         console.log(username);
-         console.log(password);
          request.open('POST','http://venisavitri.imad.hasura-app.io/login' ,true);
          request.setRequestHeader('Content-Type','application/json');
          request.send(JSON.stringify({username: username , password: password}));
