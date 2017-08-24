a4fc3f7dec7ff79bff066c65ecf08cd28a6215ed
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-24 18:02:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,29 @@
         var submit = document.getElementById('submit_btn');
-        submit.onclick=function(){
+        submit.onclick = function(){
              var request = new XMLHttpRequest();
         request.onreadystatechange = function(){
             if(request.readyState === XMLHttpRequest.DONE)
             {
                 if(request.status === 200){
-                     var username = document.getElementById('username').value;
-                     var password = document.getElementById('password').value;
-                  console.log('user logged in');
+                 
                  alert('logged in successfully');
-                }
-                else if(request.status === 403){
+                } else if(request.status === 403){
                     alert('username/password is invalid');
-                }
-               else if(request.stauts === 500){
+                } else if(request.stauts === 500){
                    alert('some internal error');
                }     
                 }
         };
+         var username = document.getElementById('username').value;
+         var password = document.getElementById('password').value;
+         console.log(username);
+         console.log(password);
          request.open('POST','http://venisavitri.imad.hasura-app.io/login' ,true);
-         request.setRequestHeader('Content-Type','application/json');
-         console.log('hai');
+         request.setRequestHeader('Content-Type', 'application/json');
          request.send(JSON.stringify({username: username , password: password}));
         
         };
 
            
-                   console.log('ok');
+            
     
