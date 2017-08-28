626998c7c8b433f71d8ec18954f2530f4bc70405
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-08-28 11:03:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,10 @@ app.get('/check_login', function(req, res)
     }
     
 });
+app.get('/logout', function(req, res){
+    delete req.session.auth;
+    res.send('you are logged out');
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
