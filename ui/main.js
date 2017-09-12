2fbecb519e801b1a5d42759278cb042c409dcee7
savitripotti <30750179+savitripotti@users.noreply.github.com>
2017-09-12 20:54:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
         var submit = document.getElementById('submit_btn');
+        
         submit.onclick = function(){
              var request = new XMLHttpRequest();
         request.onreadystatechange = function(){
@@ -14,6 +15,7 @@
                }     
                 }
         };
+        
          var username = document.getElementById('username').value;
          var password = document.getElementById('password').value;
          console.log(username);
@@ -23,6 +25,38 @@
          request.send(JSON.stringify({username: username , password: password}));
         
         };
+         var createuser = document.getElementById('createuser');
+        
+        createuser.onclick = function(){
+             var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(request.readyState === XMLHttpRequest.DONE)
+            {
+                if(request.status === 200){
+                 
+                 alert('logged in successfully');
+                } else if(request.status === 403){
+                    alert('username/password is invalid');
+                } else if(request.stauts === 500){
+                   alert('some internal error');
+               }     
+                }
+        };
+        
+         var username = document.getElementById('username').value;
+         var password = document.getElementById('password').value;
+         console.log(username);
+         console.log(password);
+         request.open('POST','http://venisavitri.imad.hasura-app.io/create-user' ,true);
+         request.setRequestHeader('Content-Type', 'application/json');
+         request.send(JSON.stringify({username: username , password: password}));
+        
+        };
+
+           
+            
+    
+
 
            
             
